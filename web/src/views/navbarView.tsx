da6ef56ff4ae84452f3ebd65a78b79b964fc9906
John Elofsson <johnelo@ug.kth.se>
2025-12-08 14:51:34 +0100

    Added navbar functionality

--- web/src/views/navbarView.tsx
+++ web/src/views/navbarView.tsx
@@ -1,24 +1,29 @@
+import { Link } from "react-router-dom";
+
 export function NavbarView() {
     return (
         <nav className="bg-white shadow-md">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-16">
                     <div className="flex items-center">
-                        <h1 className="text-xl font-bold text-gray-800">
+                        <Link to="/" className="text-xl font-bold text-gray-800 hover:text-gray-600">
                             CommutePodder
-                        </h1>
+                        </Link>
                     </div>
                     
                     <div className="hidden md:flex space-x-8">
-                        <a href="#" className="text-gray-600 hover:text-gray-900">
+                        <Link to="/" className="text-gray-600 hover:text-gray-900">
                             Home
-                        </a>
-                        <a href="#" className="text-gray-600 hover:text-gray-900">
+                        </Link>
+                        <Link to="/travel" className="text-gray-600 hover:text-gray-900">
+                            Travel
+                        </Link>
+                        <Link to="/episodes" className="text-gray-600 hover:text-gray-900">
                             Episodes
-                        </a>
-                        <a href="#" className="text-gray-600 hover:text-gray-900">
+                        </Link>
+                        <Link to="/profile" className="text-gray-600 hover:text-gray-900">
                             Profile
-                        </a>
+                        </Link>
                     </div>
                     
                     <div className="md:hidden">
